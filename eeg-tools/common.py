8e94970825a211329883d77fcb76cafe93409ef3
acpuchades <acaravacapuchades@gmail.com>
2021-09-18 23:39:55 +0200

    Refactor argument handling code

--- eeg-tools/common.py
+++ eeg-tools/common.py
@@ -1,7 +1,21 @@
+import os
 from typing import *
+from argparse import ArgumentParser
 
-def use_first_as_default(choices: List[str]) -> dict:
-	return {'choices': choices, 'default': choices[0]}
+import mne
+
+def add_global_options(parser: ArgumentParser) -> ArgumentParser:
+	parser.add_argument('-q', '--quiet', action='store_true', help='do not show messages')
+
+def add_freesurfer_options(parser: ArgumentParser) -> ArgumentParser:
+	parser.add_argument('-s', '--subject', metavar='ID', required=True, help='FreeSurfer subject ID')
+	parser.add_argument('-d', '--subjects-dir', metavar='DIR', help='FreeSurfer subjects folder path')
+	return parser
+
+def add_output_options(parser: ArgumentParser) -> ArgumentParser:
+	parser.add_argument('-z', '--gzip', action='store_true', help='use gzip to compress output data')
+	parser.add_argument('-o', '--output', metavar='NAME', required=True, help='used as template for output files')
+	return parser
 
 def make_output_filename(name: str, type: str = None, compress: bool = False) -> str:
 	fname = name
@@ -17,3 +31,16 @@ def parse_optional(t: Callable[[str], any]):
 
 def parse_with_mapping(t: Callable[[str], any], mapping: Mapping[str, any]):
 	return lambda x: mapping.get(x) or t(x)
+
+def process_freesurfer_options(args: dict) -> None:
+	subjects_dir = os.getenv('SUBJECTS_DIR')
+	if args.subjects_dir:
+		mne.set_config('SUBJECTS_DIR', args.subjects_dir)
+		subjects_dir = args.subjects_dir
+
+def process_global_options(args: dict) -> None:
+	if args.quiet:
+		mne.set_log_level('warning')
+
+def use_first_as_default(choices: List[str]) -> dict:
+	return {'choices': choices, 'default': choices[0]}
