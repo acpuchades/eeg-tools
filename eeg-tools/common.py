12106185dcf4dadc3686e29904cfb0b6b393eb63
acpuchades <acaravacapuchades@gmail.com>
2021-09-21 23:19:34 +0200

    Implement logging options

--- eeg-tools/common.py
+++ eeg-tools/common.py
@@ -4,8 +4,10 @@ from argparse import ArgumentParser
 
 import mne
 
-def add_global_options(parser: ArgumentParser) -> ArgumentParser:
-	parser.add_argument('-q', '--quiet', action='store_true', help='do not show messages')
+def add_logging_options(parser: ArgumentParser) -> ArgumentParser:
+	loglevel_opts = parser.add_mutually_exclusive_group()
+	loglevel_opts.add_argument('-q', '--quiet', action='store_true', help='do not show messages')
+	loglevel_opts.add_argument('-v', '--verbose', action='count', help='show additional info')
 
 def add_freesurfer_options(parser: ArgumentParser) -> ArgumentParser:
 	parser.add_argument('-s', '--subject', metavar='ID', required=True, help='FreeSurfer subject ID')
@@ -17,11 +19,12 @@ def add_output_options(parser: ArgumentParser) -> ArgumentParser:
 	parser.add_argument('-o', '--output', metavar='NAME', required=True, help='used as template for output files')
 	return parser
 
-def make_output_filename(name: str, type: str = None, compress: bool = False) -> str:
+def make_output_filename(name: str, type: str = None, ext: str='fif', compress: bool = False) -> str:
 	fname = name
 	if type:
 		fname += f'_{type}'
-	fname += '.fif'
+	if ext:
+		fname += f'.{ext}'
 	if compress:
 		fname += '.gz'
 	return fname
@@ -38,9 +41,14 @@ def process_freesurfer_options(args: dict) -> None:
 		mne.set_config('SUBJECTS_DIR', args.subjects_dir)
 		subjects_dir = args.subjects_dir
 
-def process_global_options(args: dict) -> None:
+def process_logging_options(args: dict) -> None:
 	if args.quiet:
-		mne.set_log_level('warning')
+		mne.set_log_level(False)
+	elif args.verbose:
+		if args.verbose == 1:
+			mne.set_log_level('INFO')
+		elif args.verbose >= 2:
+			mne.set_log_level('DEBUG')
 
 def use_first_as_default(choices: List[str]) -> dict:
 	return {'choices': choices, 'default': choices[0]}
